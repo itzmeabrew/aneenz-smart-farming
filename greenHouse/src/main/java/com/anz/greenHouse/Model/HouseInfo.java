3b16ee4b0ef07a5fff8ff4820499e151514816c3
abrew <abrew.abraham@innovaturelabs.com>
2022-03-25 17:06:55 +0530

    added house model

--- greenHouse/src/main/java/com/anz/greenHouse/Model/HouseInfo.java
+++
@@ -1,9 +0,0 @@
-package com.anz.greenHouse.Model;
-
-import javax.persistence.Entity;
-
-@Entity
-public class HouseInfo
-{
-
-}
--- greenHouse/src/main/java/com/anz/greenHouse/Service/UserServices.java
+++
@@ -1,8 +0,0 @@
-package com.anz.greenHouse.Service;
-
-import com.anz.greenHouse.Model.UserInfo;
-
-public interface UserServices
-{
-    UserInfo getUser(String userName,String password);
-}
